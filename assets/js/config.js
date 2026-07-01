5622a2557c6fae3d88279d0d1db091205dc60b8d
anbaanbaaa99-droid <anbaanbaaa99@gmail.com>
2026-07-01 19:01:26 +0700

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,17 @@
+/**
+ * ==========================================
+ * HHT ASSET MANAGEMENT
+ * CONFIG
+ * ==========================================
+ */
+
+const CONFIG = {
+
+    // URL WEB APP APPS SCRIPT
+    API_URL: "https://script.google.com/macros/s/AKfycbzlV8QgpCRK3VHbJy0VJvVq5ZqXyj98XigSi1tjFwBjNgLYX7Rnp6cDnuI_9_hSqOz9BQ/exec",
+
+    APP_NAME: "HHT Asset Management",
+
+    VERSION: "1.0.0"
+
+};
